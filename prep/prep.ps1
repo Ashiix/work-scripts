702f4560e593a22089edcec4d8b5d6419e5fbddd
Ashiix <ash@ashiix.xyz>
2024-11-06 13:56:09 -0500

    Add support for installation of vendor software, among other things

--- prep/prep.ps1
+++ prep/prep.ps1
@@ -1,11 +1,76 @@
 # PowerShell script for prepping work machines
 
+# CONFIG
+# Only installation method at present, may add a more sane one later
+$hp_install_method = "bundle"
+
 function Prep {
+    # Handle directory generation
     $data_path = "$env:script_data_path\Prep"
     if (!(Test-Path $data_path)) {
         Write-Host "Data directory not found, creating now."
         New-Item $data_path -Type Directory
-    }   
+    }
+    
+    # Configure winget
+    winget source update
+    
+    # Trust PSGallery 
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+    # Run Windows updates
+    Write-Host "Installing PSWindowsUpdate module."
+    Install-Module PSWindowsUpdate
+    Write-Host "Checking for updates..."
+    Get-WindowsUpdate
+    Write-Host "installing updates..."
+    Install-WindowsUpdate -AcceptAll
+
+    # Install vendor software 
+    $oem = (Get-WmiObject -Class Win32_ComputerSystem -Property Manufacturer).Manufacturer
+    Write-Host "Vendor identified as $oem. Proceeding with vendor software installation."
+    # Handle HP machines
+    # No official way to unattented install support assistant, using hacky method of extracting app bundle from installer executable
+    # Executable can be found here > https://support.hp.com/us-en/help/hp-support-assistant
+    if ($oem -eq "HP" -and $hp_install_method -eq "bundle") {
+        Write-Host "Installing HP Support Assist using the extracted bundle method."
+        Add-AppxPackage $data_path\hpsa.appxbundle
+    }
+    # Handle Lenovo machines
+    # Sane install method, just uses winget
+    elseif ($oem -eq "Lenovo") {
+        Write-Host "Installing Lenovo Vantage using winget."
+        winget install 9WZDNCRFJ4MV --force # MS Store code for Vantage
+    }
+    # Handle Dell machines
+    # Sane install method, just uses installer executable
+    elseif ($oem -eq "Dell") {
+        Write-Host "Installing Dell SupportAssist using executable."
+        .\$data_path\SupportAssistInstaller.exe /S
+    }
+    else {
+        Write-Host "No matching software found for $oem. Proceeding with rest of configuration."
+    }
+
+    # Run Windows updates
+
+    # Power settings configuration
+
+    # Disable UAC
+    Write-Host "Disabling UAC, device requires reboot."
+    Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
+
+    # Install DEDR
+
+    # Install standard software
+    # Chrome
+    # Firefox
+    # Reader
+
+    # Run dropin script
+    .\src\dropin.ps1
 }
 
 Prep
+
+
