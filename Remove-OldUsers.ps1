3ee7432c18523e07da5e858f4ab7bc63375c8b5d
Ash <35050991+Ashiix@users.noreply.github.com>
2025-02-28 14:43:28 -0500

    Refactor Remove-OldUsers function to improve error handling and enhance user output

--- Remove-OldUsers.ps1
+++ Remove-OldUsers.ps1
@@ -22,52 +22,60 @@ function Get-LoadTime {
         $reg_values
     )
     return [DateTime]::FromFileTime("0x$($reg_values.ProfileLoadTimeHigh.ToString('X8'))$($reg_values.ProfileLoadTimeLow.ToString('X8'))")
-} 
+}
 
-if ($history_method -eq 'lastloadtime') {
-    $standard_users = Get-CimInstance -ClassName Win32_UserProfile | Where-Object { $_.Special -eq $false }
-    $to_remove = $()
-    $standard_users | ForEach-Object {
-        $reg_key = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$($_.SID)"
-        $reg_values = Get-ItemProperty -Path $reg_key -ErrorAction SilentlyContinue
-        $load_time = 0
-        try {
-            Write-Output 'Attempting local profile load time.'
-            $load_time = Get-LocalLoadTime($reg_values)
-            Write-Output 'Using local profile load time.'
-        }
-        catch {
+function Remove-OldUsers {
+    param (
+        $history_method,
+        $remove_older_than
+    )
+    if ($history_method -eq 'lastloadtime') {
+        $standard_users = Get-CimInstance -ClassName Win32_UserProfile | Where-Object { $_.Special -eq $false }
+        $to_remove = $()
+        $standard_users | ForEach-Object {
+            $reg_key = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$($_.SID)"
+            $reg_values = Get-ItemProperty -Path $reg_key -ErrorAction SilentlyContinue
+            $load_time = 0
             try {
-                Write-Output 'Failed...'
-                Write-Output 'Attempting profile load time.'
-                $load_time = Get-LoadTime($reg_values)
-                Write-Output 'Using profile load time.'
+                Write-Output 'Attempting local profile load time.'
+                $load_time = Get-LocalLoadTime($reg_values)
+                Write-Output 'Using local profile load time.'
             }
             catch {
-                Write-Host "Failed, skipping user.`n"
-                $load_time = 0
+                try {
+                    Write-Output 'Failed...'
+                    Write-Output 'Attempting profile load time.'
+                    $load_time = Get-LoadTime($reg_values)
+                    Write-Output 'Using profile load time.'
+                }
+                catch {
+                    Write-Host "Failed, skipping user.`n"
+                    $load_time = 0
+                }
             }
-        }
-        if (-not $load_time -eq 0) {
-            $days_since = ([DateTime]::Now - $load_time).Days
-            $directory_size = $([Math]::Round((Get-ChildItem $_.LocalPath -Recurse -Force | Measure-Object -Property Length -Sum).Sum / 1GB, 2))
-            Write-Output $_.LocalPath.Split('\')[-1]
-            Write-Output "$load_time"
-            Write-Output "$directory_size GB"
-            Write-Output "$days_since days since last used."
-            Write-Output ''
-            if ($days_since -ge $remove_older_than) {
-                $to_remove += @($_)
+            if (-not $load_time -eq 0) {
+                $days_since = ([DateTime]::Now - $load_time).Days
+                $directory_size = $([Math]::Round((Get-ChildItem $_.LocalPath -Recurse -Force | Measure-Object -Property Length -Sum).Sum / 1GB, 2))
+                Write-Output $_.LocalPath.Split('\')[-1]
+                Write-Output "$load_time"
+                Write-Output "$directory_size GB"
+                Write-Output "$days_since days since last used."
+                Write-Output ''
+                if ($days_since -ge $remove_older_than) {
+                    $to_remove += @($_)
+                }
             }
         }
     }
+
+    Write-Output "Removing users older than $remove_older_than days..."
+    $to_remove | ForEach-Object {
+        Write-Output $_.LocalPath.Split('\')[-1]
+        $_ | Remove-CimInstance -WhatIf
+    }
 }
 
-Write-Output 'Removing users...:'
-$to_remove | ForEach-Object {
-    Write-Output $_.LocalPath.Split('\')[-1]
-    $_ | Remove-CimInstance -WhatIf
-}
+Remove-OldUsers $history_method $remove_older_than
 # elseif ($history_method -eq 'ntuser.dat') {
 #     Write-Output 'Using ntuser.dat method...'
 #     $standard_users = Get-CimInstance -ClassName Win32_UserProfile | Where-Object { $_.Special -eq $false }
