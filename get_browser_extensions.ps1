9795f52afc65121210dcdfea3c0aabc8c950d52d
Ash <35050991+Ashiix@users.noreply.github.com>
2025-03-28 14:07:40 -0400

    Add functionality to write browser extensions data to Access database

--- get_browser_extensions.ps1
+++ get_browser_extensions.ps1
@@ -2,18 +2,42 @@
 # PowerShell script for collecting all installed extensions for both Chromium and Firefox-based browsers across all user and browser profiles
 
 # For once nothing to configure
+# Scratch that, where's the Access database you wanna save to.
+$db_path = $env:extension_db_path
 
-# Open DB connection once
-# Update DB in real time
-# Close DB connection
-# MS access DB
-
+# General functions
 function retrieve_user_profiles {
     $user_profiles = @()
     Get-ChildItem C:\Users\ | ForEach-Object {
         $user_profiles += $_.FullName
     }
     return $user_profiles
+}
+function write_to_access_db {
+    param (
+        $extensions
+    )
+    try {
+        $db_connection = New-Object System.Data.OleDb.OleDbConnection
+        $db_connection.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=$db_path"
+        Write-Host "Opening connection to Access DB..."
+        $db_connection.Open()
+        $db_command = New-Object System.Data.OleDb.OleDbCommand
+        $db_command.Connection = $db_connection
+        foreach ($extension in $extensions) {
+            Write-Host "Write to DB: $extension"
+            $db_command.CommandText = "INSERT INTO BrowserExtensions (ExtensionName, Endpoint, UserProfile, Browser, ScanDate) VALUES ('$($extension.Name)', '$($extension.Endpoint)', '$($extension.UserProfile)', '$($extension.Browser)', Date())"
+            $db_command.ExecuteNonQuery() | Out-Null
+        }
+        Write-Host "Done writing entries."
+    }
+    catch {
+        Write-Error "$_"
+    }
+    finally {
+        $db_connection.Close()
+        Write-Host "Connection to Access DB closed."
+    }
 }
 
 # Function for handling Google Chrome
@@ -62,7 +86,7 @@ function retrieve_chromium_extensions {
         $browser_profile_dir
     )
     $extension_ids = @()
-    $extension_names = @()
+    $extensions = @()
     $excluded_names = @('__MSG_extName__', '__MSG_APP_NAME__', '__MSG_fullName__', 'Edge relevant text changes')
     Get-ChildItem $browser_profile_dir\Extensions | ForEach-Object {
         if ($_.Name -ne 'Temp') {
@@ -73,11 +97,18 @@ function retrieve_chromium_extensions {
         $manifest = $(Get-Content $extension_path\*\manifest.json -Raw | ConvertFrom-Json) 
         foreach ($property in $manifest.PSObject.Properties) {
             if (('short_name' -match $property.Name) -and ($property.Value -notin $excluded_names)) {
-                $extension_names += $property.Value
+                $user_profile = $browser_profile_dir.Substring(0, $($browser_profile_dir.IndexOf('\AppData')))
+                $extension = [PSCustomObject]@{
+                    Name        = $property.Value
+                    Endpoint    = $env:COMPUTERNAME
+                    UserProfile = $user_profile
+                    Browser     = 'Chromium'
+                }
+                $extensions += $extension
             }
         }
     }
-    return $extension_names
+    return $extensions
 }
 
 # Functions for handling Firefox
@@ -85,28 +116,34 @@ function retrieve_firefox_extensions {
     param (
         $user_profiles
     )
-    $extension_names = @()
+    $extensions = @()
     $excluded_names = @('Form Autofill', 'Picture-In-Picture', 'Firefox Screenshots', 'WebCompat Reporter', 'Web Compatibility Interventions', 'Add-ons Search Detection', 'Light', 'Dark', 'Firefox Alpenglow', 'Fix add-ons signed before 2018 (Bug 1954818)')
-    $user_profiles | ForEach-Object {
-        if (Test-Path $_\AppData\Roaming\Mozilla\Firefox\Profiles) {
-            Get-ChildItem "$_\AppData\Roaming\Mozilla\Firefox\Profiles\" | ForEach-Object {
+    foreach ($profile in $user_profiles) {
+        if (Test-Path $profile\AppData\Roaming\Mozilla\Firefox\Profiles) {
+            Get-ChildItem "$profile\AppData\Roaming\Mozilla\Firefox\Profiles\" | ForEach-Object {
                 if (Test-Path "$($_.FullName)\extensions.json") {
                     $extensions_data = Get-Content "$($_.FullName)\extensions.json" -Raw | ConvertFrom-Json
                     $extensions_data.addons | ForEach-Object {
                         if ($_.defaultLocale.name -and ($_.defaultLocale.name -notin $excluded_names) -and ($_.defaultLocale.name -notlike 'System theme*')) {
-                            $extension_names += $_.defaultLocale.name
+                            $extension = [PSCustomObject]@{
+                                Name        = $_.defaultLocale.name
+                                Endpoint    = $env:COMPUTERNAME
+                                UserProfile = $profile
+                                Browser     = 'Firefox'
+                            }
+                            $extensions += $extension
                         }
                     }
                 }
             }
         }
     }
-    return $extension_names
+    return $extensions
 }
 
 # Main function
 function get_browser_extensions {
-    $all_extensions = @()
+    $extensions = @()
 
     # Handle Google Chrome
     $chrome_profiles = @()
@@ -115,8 +152,9 @@ function get_browser_extensions {
     }
     $chrome_profiles | ForEach-Object {
         retrieve_chromium_extensions $_ | ForEach-Object {
-            if ($_ -notin $all_extensions) {
-                $all_extensions += $_
+            $_.Browser = 'Chrome'
+            if ($_ -notin $extensions) {
+                $extensions += $_
             }
         }
     }
@@ -128,21 +166,24 @@ function get_browser_extensions {
     }
     $edge_profiles | ForEach-Object {
         retrieve_chromium_extensions $_ | ForEach-Object {
-            if ($_ -notin $all_extensions) {
-                $all_extensions += $_
+            $_.Browser = 'Edge'
+            if ($_ -notin $extensions) {
+                $extensions += $_
             }
         }
     }
 
     # Handle Firefox
     retrieve_firefox_extensions $(retrieve_user_profiles) | ForEach-Object {
-        if ($_ -notin $all_extensions) {
-            $all_extensions += $_
+        if ($_ -notin $extensions) {
+            $extensions += $_
         }
     }
     
     # Return all retrieved extensions across all browsers and profiles
-    return $all_extensions
+    return $extensions
 }
 
-get_browser_extensions
+$extensions = $(get_browser_extensions)
+
+write_to_access_db $extensions
